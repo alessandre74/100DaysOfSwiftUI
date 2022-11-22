697f21f6bdada9351e167a7a378c8cc6191d479c
Alessandre Livramento <aleliv74@gmail.com>
2022-11-22 00:36:11 -0300

    Curso 100 Days of SwiftUI

---
+++ 04 - Escalando para aplicativos maiores/20 - AnimatablePairView/AnimatablePairView/AnimatablePairViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatablePairViewApp.swift
+//  AnimatablePairView
+//
+//  Created by Alessandre Livramento on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatablePairViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
