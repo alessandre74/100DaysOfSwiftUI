011913d21c43055f1d2f532c48b82448dc883e0f
Alessandre Livramento <aleliv74@gmail.com>
2022-12-02 11:55:24 -0300

    Curso 100 Days of SwiftUI

--- 03 - Expandindo suas habilidades/02 - Strings/stringView/ContentView.swift
+++ 03 - Expandindo suas habilidades/02 - Strings/stringView/ContentView.swift
@@ -16,33 +16,29 @@ struct ContentView: View {
 struct ListView1: View {
     var body: some View {
         List {
-            
             Section("Section 1") {
                 Text("Static row 1")
                 Text("Static row 1")
             }
-            
-            Section ("Section 2") {
-                ForEach(0..<5) {
+
+            Section("Section 2") {
+                ForEach(0 ..< 5) {
                     Text("Dynamic row \($0)")
                 }
             }
-            
-            
-            Section ("Section")  {
+
+            Section("Section") {
                 Text("Static row 3")
                 Text("Static row 4")
             }
-            
         }
         .listStyle(.grouped)
-        
     }
 }
 
 struct ListView2: View {
-    var body: some View{
-        List(0..<5) {
+    var body: some View {
+        List(0 ..< 5) {
             Text("Dynamic row \($0)")
         }
     }
@@ -50,20 +46,20 @@ struct ListView2: View {
 
 struct ListView3: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
-    
+
     var body: some View {
         VStack {
-            List(people, id: \.self){
+            List(people, id: \.self) {
                 Text($0)
             }
-            
+
             List {
                 Text("Static Row")
-                
-                ForEach(people, id: \.self){
+
+                ForEach(people, id: \.self) {
                     Text($0)
                 }
-                
+
                 Text("Static row")
             }
         }
@@ -75,50 +71,48 @@ struct WorkingStringsView: View {
         VStack {
             Button {
                 print(StringLetters())
-                
+
             } label: {
                 Text("Clique aqui")
             }
         }
     }
-    
+
     func StringLetters() -> Bool {
-        
-        let input = "A l e s s a n d r e"
-        let letters = input.components(separatedBy: " ") // Divide em um array com base em separatedBy.
-        
-        let letter = letters.randomElement() //Obtém uma letra aleatória do array
-        
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "" //Limpas espaços iniciais e finais de uma string
-        
+//        let input = "A l e s s a n d r e"
+//        let letters = input.components(separatedBy: " ") // Divide em um array com base em separatedBy.
+
+//        let letter = letters.randomElement() // Obtém uma letra aleatória do array
+
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "" // Limpas espaços iniciais e finais de uma string
+
         let word = "Specing"
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelleRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         let allGood = misspelleRange.location == NSNotFound
-     
+
         return allGood
     }
 }
 
 struct LoadFileView: View {
     var body: some View {
-        Button{
+        Button {
             print(loadFile())
         } label: {
             Text("Carregar arquivo..")
         }
     }
-    
+
     func loadFile() -> String {
         guard let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") else {
             return "arquivo vazio"
         }
-        
+
         guard let fileContents = try? String(contentsOf: fileURL) else { return "arquivo vazio" }
-        
+
         return fileContents
-        
     }
 }
 
@@ -127,4 +121,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
